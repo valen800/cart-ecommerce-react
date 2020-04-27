2affdcd68a77dcac34ea54da15f016a125069fcb
valen800 <varulo.cvcv@gmail.com>
2020-04-27 08:19:34 +0200

    Dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "./App.css";
 
 function App() {
+  const openToast = () => {
+    toast("Este es mi toast");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openToast}>Click</button>
+      <ToastContainer />
     </div>
   );
 }
